225f626297487ef627d139368fbb4905cfac2308
PointedPine <j06.k1ps@gmail.com>
2022-01-12 14:45:23 +0900

    지킬 업데이트

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -586,4 +586,10 @@ var store = [{
         "tags": ["지사","월별 매출관리"],
         "url": "/branch%20sm/MSM/",
         "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["mysite","jekyll","update"],
+        "tags": [],
+        "url": "/mysite/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
       }]
